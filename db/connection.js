b083fb4e11ff319bfb036ab9bb758f8e1fb91545
mohit9450 <mohitgoyalbxn@gmail.com>
2021-08-18 13:46:23 -0800

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/fb_chat" ,{useNewUrlParser:true, useUnifiedTopology:true}).then(() =>{
+mongoose.connect("mongodb+srv://mohit_05:9483726161@crazyking.z3zkr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" ,{useNewUrlParser:true, useUnifiedTopology:true}).then(() =>{
     console.log("cinnection succefully....");
 }).catch((err)=>{
     console.log(err);
