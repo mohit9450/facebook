fee76271ebe710202b5dea8b3e95b8a38fe906a7
mohit9450 <mohitgoyalbxn@gmail.com>
2021-08-21 08:28:48 -0800

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const express = require('express');
 const url = require('url');
 const {Server} = require('socket.io-client');
 const app = express();
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+    next();
+});
 const server = http.createServer(app);
 const fs = require('fs');
 const io= require('socket.io')(server,{
